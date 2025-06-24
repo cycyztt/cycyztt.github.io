a4083ec76d210340272eed3905df136aa6e87d25
cycyztt <daizhifeng23@gmail.com>
2025-06-24 11:36:06 +0800

    Site updated: 2025-06-24 11:36:05

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/22/hello-world/","2023/10/15/我的产品/"];function toRandomPost(){
+var posts=["2023/10/15/我的产品/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
